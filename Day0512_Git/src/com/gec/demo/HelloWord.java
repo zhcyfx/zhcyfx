4a6700a6216f7ca737dc922f2847cd3cb20d3895
zhcyfx <3335462957@qq.com>
2021-05-12 13:51:17 +0800

    超凡王者之旅...

--- Day0512_Git/src/com/gec/demo/HelloWord.java
+++ Day0512_Git/src/com/gec/demo/HelloWord.java
@@ -1,6 +1,10 @@
 package com.gec.demo;
 
 public class HelloWord {
+    public static void addUser(){
+        System.out.println("超凡入圣,太迷了...");
+    }
+
     public static void main(String[] args) {
         System.out.println("寂寞哥,杨汉...");
         System.out.println("寂寞哥,杨汉...");
@@ -20,5 +24,9 @@ public class HelloWord {
         System.out.println("沙雕之楷之骚操作...");
         System.out.println("沙雕之楷之骚操作...");
         System.out.println("沙雕之楷之骚操作...");
+
+        System.out.println("--------------------------------");
+
+        addUser();
     }
 }
