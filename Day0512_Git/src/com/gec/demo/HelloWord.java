e2fafcaffa7faf86f7bb321b3affa06c404acdfa
zhcyfx <3335462957@qq.com>
2021-05-12 12:37:12 +0800

    寂寞哥之杨汉骚操作...

--- Day0512_Git/src/com/gec/demo/HelloWord.java
+++ Day0512_Git/src/com/gec/demo/HelloWord.java
@@ -3,5 +3,12 @@ package com.gec.demo;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("寂寞哥,杨汉...");
+        System.out.println("寂寞哥,杨汉...");
+        System.out.println("寂寞哥,杨汉...");
+        System.out.println("寂寞哥,杨汉...");
+        System.out.println("寂寞哥,杨汉...");
+        System.out.println("寂寞哥,杨汉...");
+        System.out.println("寂寞哥,杨汉...");
+        System.out.println("寂寞哥,杨汉...");
     }
 }
